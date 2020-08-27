812c66aa01440f8e3423a1204b7c1323aac9b7cf
M4cheal <527059095@qq.com>
2020-08-27 22:28:28 +0800

    第二章：整型

--- C_fishc/2.DataType/int.c
+++ C_fishc/2.DataType/int.c
@@ -1,13 +1,39 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-#define PRICE 30
+// #define PRICE 30
+
+// void main()
+// {
+//     int num, total;
+//     num = 10;
+//     total  = num * PRICE;
+//     printf("total = %d\n", sizeof(float));
+// }
+
+// #include <stdio.h>
+
+// void main()
+// {
+//     int a, b, c, d;
+//     unsigned u;
+//     a = 12;
+//     b = -24;
+//     u = 10;
+//     c = a + u;
+//     d = b + u;
+//     printf("a + u = %d, d + u = %d \n", c, d);
+// }
+
+
+/**
+ * 整型溢出
+*/
+#include <stdio.h>
 
 void main()
 {
-    int num, total;
-    num = 10;
-    total  = num * PRICE;
-    printf("total = %d\n", total);
-
-
+     int a, b;
+    a = 32767;
+    b = a + 1;
+    printf("%d, %d", a, b);
 }
