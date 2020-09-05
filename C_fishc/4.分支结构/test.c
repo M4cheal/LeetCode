0c5daf0544d16d36601c198aa3682ac4af60c1f7
M4cheal <527059095@qq.com>
2020-09-05 22:58:19 +0800

    第四章

--- C_fishc/4.分支结构/test.c
+++ C_fishc/4.分支结构/test.c
@@ -1,17 +1,72 @@
-#include <stdio.h>
+// #include <stdio.h>
 
+// void main()
+// {
+//     int a, b, max;
+//     printf("\n input two numbers:");
+//     scanf("%d%d", &a, &b);
+//     if (a > b)
+//     {
+//         printf("max = %d", a);
+//     }else
+//     {
+//         printf("max = %d", b);
+//     }
+// }
+
+/***
+ * 插入一个分数score
+ */
+// #include <stdio.h>
+// void main()
+// {
+//     int score;
+//     printf("input score");
+//     scanf("%d", &score);
+//     if (score >= 90)
+//     {
+//         printf("A");
+//     }else if (score >= 80 && score < 90)
+//     {
+//         printf("B");
+//     }else if (score >= 70 && score < 80)
+//     {
+//         printf("C");
+//     }else if (score >= 60 && score < 70)
+//     {
+//         printf("D");
+//     }else
+//     {
+//         printf("E");
+//     }
+// }
+
+
+/**
+ * 输入三个数a, b, c, 要求按从小到大的顺序输出。
+ */
+#include <stdio.h>
 void main()
 {
-    int a, b, max;
-    printf("\n input two numbers:");
-    scanf("%d%d", &a, &b);
+    int a, b, c, temp;
+    scanf("%d%d%d", &a, &b, &c);
     if (a > b)
     {
-        printf("max = %d", a);
-    }else
-    {
-        printf("max = %d", b);
+        temp = a;
+        a = b;
+        b = temp;
     }
-    
-
+    if (a > c)
+    {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+    if (b > c)
+    {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+    printf("%d%d%d", a, b, c);
 }
