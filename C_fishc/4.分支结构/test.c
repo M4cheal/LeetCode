b577e7748a37c02621dd23c16c76fccc1993118d
M4cheal <527059095@qq.com>
2020-09-05 22:24:01 +0800

    第四章

--- C_fishc/4.分支结构/test.c
+++ C_fishc/4.分支结构/test.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+
 void main()
 {
-    int a = 6 > 5;
-    printf("%d", a);
+    int a, b, max;
+    printf("\n input two numbers:");
+    scanf("%d%d", &a, &b);
+    if (a > b)
+    {
+        printf("max = %d", a);
+    }else
+    {
+        printf("max = %d", b);
+    }
+    
+
 }
