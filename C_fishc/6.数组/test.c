c41a5ede3c1503ea72927bd2d60e8a3a4374a788
M4cheal <527059095@qq.com>
2020-09-25 01:10:52 +0800

    目录

--- C_fishc/6.数组/test.c
+++ C_fishc/6.数组/test.c
@@ -30,31 +30,76 @@
 // }
 
 // 冒泡排序
+// #include <stdio.h>
+// void main()
+// {
+//     int a[10];
+//     printf("input 10 number:\n");
+//     for (int i = 0; i < 10; i++)
+//     {
+//         scanf("%d",&a[i]);
+//     }
+//     printf("\n");
+
+//     for (int j = 10; j > 0; j--)
+//     {
+//         for (int i = 0; i < j - 1; i++)
+//         {
+//             if (a[i] > a[i+1])
+//             {
+//                 int c = a[i];
+//                 a[i] = a[i+1];
+//                 a[i+1] = c;
+//             }
+//         }
+//     }
+//     for (int i = 0; i < 10; i++)
+//     {
+//         printf("%d,", a[i]);
+//     }
+// }
+
+
+// 二分法
 #include <stdio.h>
+#define M 10
 void main()
 {
-    int a[10];
-    printf("input 10 number:\n");
-    for (int i = 0; i < 10; i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    printf("\n");
+    static int a[M] = {-12, 0, 6, 16,
+                        23, 56, 80, 100, 110, 115};
+    int n, low, mid, high, found;
+    low = 0;
+    high = M - 1;
+    found = 0;
+    printf("please input a number:\n");
 
-    for (int j = 10; j > 0; j--)
+    while (scanf("%d", &n) != 1)
     {
-        for (int i = 0; i < j - 1; i++)
+        printf("Illegal input!!\nPlease input again!!\n");
+        getchar();// 消耗一个流，防止scanf直接读以前的流，导致无法输入
+    }
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (a[mid] == n)
         {
-            if (a[i] > a[i+1])
-            {
-                int c = a[i];
-                a[i] = a[i+1];
-                a[i+1] = c;
-            }
+            found = 1;
+            break;
+        }
+        else if (n > a[mid])
+        {
+            low = mid + 1;
+        }else
+        {
+            high = mid - 1;
         }
     }
-    for (int i = 0; i < 10; i++)
+    if (found == 1)
     {
-        printf("%d,", a[i]);
+        printf("The index of %d is %d", n, mid);
     }
+    else
+    {
+        printf("There is not %d", n);
+    }                   
 }
